be4fac031bdd65fcb0807f8236aea6bbebd5529e
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2021-06-02 13:54:24 +0200

    adds fee input

--- src/components/organisms/AddLiquidityForm.tsx
+++ src/components/organisms/AddLiquidityForm.tsx
@@ -1,13 +1,21 @@
 import { Button } from '@chakra-ui/button';
 import { FormControl } from '@chakra-ui/form-control';
 import { Input, InputGroup } from '@chakra-ui/input';
-import { Box, Flex, Text } from '@chakra-ui/layout';
+import { Box, Flex, Stack, Text } from '@chakra-ui/layout';
+import { Radio, RadioGroup } from '@chakra-ui/radio';
 import React, { FormEvent, useEffect, useMemo, useState } from 'react';
 
-import { setField, sha256hash } from '../../helpers';
+import { setField, setNumericalField } from '../../helpers';
 import { Pool } from '../../lib/Pool';
 import { Token } from '../../lib/Token';
 import TokenValueChooser from '../molecules/TokenValueChooser';
+
+const predefinedFees = {
+  '0': 0.0,
+  '0.05': 0.05,
+  '0.3': 0.3,
+  '1': 1,
+};
 
 export default function AddLiquidityForm({
   address,
@@ -25,14 +33,20 @@ export default function AddLiquidityForm({
 
   const [amt1, setAmt1] = useState(0);
   const [amt2, setAmt2] = useState(0);
+  const [newPoolFee, setNewPoolFee] = useState<string | undefined>('0');
 
   const [pool, setPool] = useState<Pool>();
 
-  const onSubmit = async (e: FormEvent) => {
+  const onSubmit = (e: FormEvent) => {
     e.preventDefault();
     if (!pool) {
-      const poolAddress = await sha256hash(firstToken!.symbol + secondToken!.symbol);
-      const p = new Pool(`0x${poolAddress}`, firstToken!, secondToken!, 0);
+      const poolFee = predefinedFees[newPoolFee || '0'] || 0;
+      const p = new Pool(
+        `0x${firstToken?.symbol.toLowerCase()}${secondToken?.symbol.toLowerCase()}pool`,
+        firstToken!,
+        secondToken!,
+        poolFee,
+      );
       p.addLiquidity(address, amt1, amt2);
       poolAdded(p);
       setPool(p);
@@ -60,9 +74,13 @@ export default function AddLiquidityForm({
     if (!firstToken || !secondToken) return false;
     if (amt1 == 0 || amt2 == 0) return false;
 
-    if (amt1 < firstToken.balanceOf(address) && amt2 < secondToken.balanceOf(address))
+    if (amt1 <= firstToken.balanceOf(address) && amt2 <= secondToken.balanceOf(address))
       return true;
   }, [firstToken, secondToken, amt1, amt2]);
+
+  const createsNewPool = useMemo<boolean | undefined>(() => {
+    return firstToken && secondToken && !pool;
+  }, [firstToken, secondToken, pool]);
 
   return (
     <Flex
@@ -117,10 +135,24 @@ export default function AddLiquidityForm({
             </InputGroup>
           </FormControl>
         </TokenValueChooser>
-        <Text color="gray.500" align="right" my={2}>
-          {pool && pool.poolToken.symbol}
-          {firstToken && secondToken && !pool && <Text>creates a new pool</Text>}
-        </Text>
+        {createsNewPool && (
+          <Flex direction="column">
+            <Text color="gray.500" align="start" my={2}>
+              choose a pool fee
+            </Text>
+            <RadioGroup onChange={setNewPoolFee} value={newPoolFee}>
+              <Stack direction="row" spacing={6}>
+                {Object.keys(predefinedFees)
+                  .sort()
+                  .map((fee) => (
+                    <Radio key={`np-fee-${fee}`} value={fee}>
+                      <Text whiteSpace="nowrap">{`${fee} %`}</Text>
+                    </Radio>
+                  ))}
+              </Stack>
+            </RadioGroup>
+          </Flex>
+        )}
       </Flex>
 
       <Button
